74fabd8873811c715c42f566e69422f5706ca682
lwwtl <598449595@qq.com>
2020-12-09 22:27:23 +0800

    add files

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import About from "../views/About";
+import Home from "../views/Home";
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+
 
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/',
+      name:'Home',
+      component:Home
+    }, { path: '/about',
+      name:'About',
+      component:About
+    }, { path: '/login',
+      name:'Login',
+      component:Login
+    },
+  ]
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,97 @@
+<template>
+    <!--登录表单的容器-->
+    <div class="login_container">
+        <!--登录区域-->
+        <div class="login_box">
+        <!--头像-->
+            <div class="avatar_box">
+                <img src="../assets/img/timg.gif" >
+            </div>
+        <!--表单-->
+            <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="100px" class="login_Form">
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="el-icon-lock"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('loginForm')">立即创建</el-button>
+                    <el-button @click="resetForm('loginForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Login',
+        data() {
+            return {
+                loginForm: {
+                    username: '',
+                    password: ''
+                },
+                loginRules: {
+                    username: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' },
+                        { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '请输入密码', trigger: 'blur' },
+                        { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    .login_container{
+    height: 100%;
+    background-color:aquamarine ;
+    }
+    .login_box{
+        width: 450px;
+        height: 380px;
+        background-color: #FFFFFF;
+        border-radius: 3px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+    }
+    .avatar_box{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #EEEEEE;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 2px 12px 0 rgba(0,0,0,0.1);
+        margin: -65px auto;
+        background-color: #FFFFFF;
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #EEEEEE;
+        }
+    }
+</style>
