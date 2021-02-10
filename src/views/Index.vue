7b5d16aaa5d47c104360b4f59259654849f0fdb6
lwwtl <598449595@qq.com>
2021-02-10 21:48:28 +0800

    首页数据渲染

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -31,7 +31,7 @@
             <div class="carCount">
                 <div class="count">使用量</div>
                 <div class="circle">
-                <el-progress type="circle" :percentage="carRate" color="orange"></el-progress>
+                <el-progress type="circle" :percentage="this.carRate" color="orange"></el-progress>
                 </div>
             </div>
         </div>
@@ -53,15 +53,29 @@
                 userNum:'0',
                 storeNum:'0',
                 carNum:'0',
-                carRate:0
-
+                rent:'0',
+                carRate:1,
+                foldXData:[],
+                foldYData:[],
+                foldList:[],
+                lineXData:[],
+                lineYData:[],
+                LineList:[],
             }
         },
         methods: {
             drawLine(){
+
                 // 基于准备好的dom，初始化echarts实例
                 const chart = this.$refs.chart
                 let myChart = this.$echarts.init(chart)
+                const _this = this
+                this.$axios.get("/order/drawLine").then(res => {
+                        _this.LineList = res.data.data
+                        _this.LineList.forEach((item,i) => {
+                            _this.lineXData[i] = _this.LineList[i].name
+                            _this.lineYData[i] = _this.LineList[i].total
+                        })
                 // 绘制图表
                 myChart.setOption({
                     color: ['#2fbeb1'],
@@ -87,7 +101,7 @@
                             show:true,
                             color:'#7b7b98',
                         },
-                        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","裤子","裤子","裤子","袜子"]
+                        data: this.lineXData
                     },
                     yAxis: {
                         splitLine:{show:false},//网格线是否显示
@@ -105,27 +119,39 @@
                         name: '租赁次数',
                         type: 'bar',
                         barWidth: '70%',
-                        data: [5, 20, 36, 10, 10, 20],
+                        data: this.lineYData
 
                     }]
                 });
+                })
             },
             drawFold(){
                 // 基于准备好的dom，初始化echarts实例
                 const chart = this.$refs.orderChart
                 let myChart = this.$echarts.init(chart)
+                const _this = this
+                this.$axios.get("/order/drawFold").then(res => {
+                    _this.foldList = res.data.data
+                    _this.foldList.forEach((item,i) => {
+                        _this.foldXData[i] = _this.foldList[i].days.substr
+                        (5,8);
+                        _this.foldYData[i] = _this.foldList[i].quantity
+                    })
+
                 // 绘制图表
                 myChart.setOption({
                     tooltip: {
                         trigger: 'axis',
                         // 自定义悬浮框展示的内容。此json对象的内容可自定义，我只是举个例子。{name: '内容标题'， value：'可以是任意字符，根据业务需求来吧'}
                         formatter(params) {
-                            return `内容标题：${params[0].data.name} 数据：${params[0].data.value}`
+                            //console.dir可以查看obj的内容
+                            return `日期：${params[0].name}
+                            成交量：${params[0].value}`
                         }
                     },
                     title: {
                         left:'center',
-                        text: '近期租赁订单数据',
+                        text: '近一个月租赁订单数据',
                         textStyle:{
                             color:'#666',
                             fontSize:28, //字体大小
@@ -133,7 +159,7 @@
                     },
                     xAxis: {
                         type: 'category',
-                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                        data: this.foldXData,
                         axisTick: {//刻度
                             show:false,
                             alignWithLabel: true
@@ -158,7 +184,8 @@
                         },
                     },
                     series: [{
-                        data: [820, 932, 901, 934, 1290, 1330, 1320],
+                        // data: [820, 932, 901, 934, 1290, 1330, 1320, 932, 901, 934, 1290, 1330, 1320, 932, 901, 934, 1290, 1330, 1320, 932, 901, 934, 1290, 1330],
+                        data: this.foldYData,
                         type: 'line',
                         smooth: false, // 是否平滑 也可以设置为 0 到 1 的值，表示平滑程度。
                         symbol: 'circle', //折线点设置为实心点
@@ -173,9 +200,37 @@
                         }
                     }]
                 });
-            }
+                })
+            },
+            useRate(){
+                const _this = this
+                this.$axios.get("/car/carRate").then(res => {
+                    const temp = res.data.data
+                    console.log(temp)
+                    let t =temp.substr(0,temp.indexOf(","))
+                    let r =temp.substr(temp.indexOf(",")+1,temp.length)
+                    _this.carNum = t
+                    _this.carRate = Math.round(r/t*100)
+                })
+            },
+            storeCount(){
+                const _this = this
+                this.$axios.get("/store/storeNum").then(res => {
+                    _this.storeNum = res.data.data
+                })
+            },
+             userCount(){
+                const _this = this
+                this.$axios.get("/user/userNum").then(res => {
+                    _this.userNum = res.data.data
+                })
+            },
+
         },
         mounted(){
+            this.useRate()
+            this.storeCount()
+            this.userCount()
             this.$nextTick(() => {
                 this.drawLine();
                 this.drawFold();
